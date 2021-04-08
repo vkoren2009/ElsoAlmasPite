e2d6a0cb106405c4f94ce1cbcf2dbcb5cd089a68
vkoren2009 <82147280+vkoren2009@users.noreply.github.com>
2021-04-08 11:24:32 +0100

    Update konyvtar.py

--- konyvtar.py
+++ konyvtar.py
@@ -1,2 +1,9 @@
-print("Add meg az elso szerzot es cimet")
-print("Hello World")
+print("Hello and Welcome to My Library")
+author=[]
+title=[]
+ 
+author.append(input("Who is the author?"))
+title.append(input("What is the title?"))
+
+for x in author: print(x)
+for x in title: print(x)
