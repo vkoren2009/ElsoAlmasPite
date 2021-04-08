4a263c9b3831b4065d1fa16e86a010cce8aeee3c
vkoren2009 <82147280+vkoren2009@users.noreply.github.com>
2021-04-08 12:02:52 +0100

    Update konyvtar.py

--- konyvtar.py
+++ konyvtar.py
@@ -4,16 +4,16 @@ title=[]
 
 author.append(input("Who is the 1. author?"))
 title.append(input("What is the 1. title?"))
-
 x=0
+
 while author[x] > "" :
   author.append(input("Who is the next author?"))
   title.append(input("What is the next title?"))
   x=x+1
 
-print("Thanks for your input. Here are your submissions:")
+print(x)
 
 x=0
 while author[x] > "" :
-  print(str(x+1) + ". " + author[x] + " : " + title[x])
-  x=x+1
+    print(author[x] + " : " + title[x])
+    x=x+1
